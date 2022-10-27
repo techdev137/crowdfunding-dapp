13b85cb51c6426e5a12ebeeb127e4a41fefcc4b6
mtg137 <jony374@outlook.com>
2022-10-27 10:11:46 -0400

    rewrite api db campaign

--- dapp/client/src/components/utils/TimeFormatter.jsx
+++
@@ -1,6 +0,0 @@
-//import React from 'react';
-const TimeFormatter = (props) => {
-    return new Date(props.time).toLocaleString("en-US", { timeZone: "Asia/Bangkok" });
-}
-
-export default TimeFormatter;
